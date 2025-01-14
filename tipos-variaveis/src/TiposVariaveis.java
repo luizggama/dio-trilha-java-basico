8a9dc29e4ed53112ee0e6a8d885bf3ae774909ae
Luiz Gustavo Gama Souza <luizggamasouza@gmail.com>
2025-01-14 13:52:20 -0300

    Alterando nome do arquivo e testando com variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,10 @@
+import javax.print.DocFlavor.STRING;
+
+public class TiposVariaveis {
+
+    int salarioMinimo;
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+        System.out.print("Seu salário será de: " + salarioMinimo);
+    }
+}
