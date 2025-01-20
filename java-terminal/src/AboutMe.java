adf0d2348bb5ca57d322730add817c281ce272ec
Luiz Gustavo Gama Souza <luizggamasouza@gmail.com>
2025-01-20 09:16:14 -0300

    Commit Do Java Para Terminal

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,5 +1,13 @@
 public class AboutMe {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        String nome = args[0];
+        String sobrenome = args[1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = double.valueOf(args[3]);
+
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + " cm");
+
     }
 }
